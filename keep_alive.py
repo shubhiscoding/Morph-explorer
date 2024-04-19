4ab4b9749b29ca6ecbcaa4b143224299e6799cda
Shubh Kesharwani <95687419+shubhiscoding@users.noreply.github.com>
2024-04-19 20:22:24 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,8 +8,8 @@ def index():
     return "Alive"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0',port=8080)
 
-def keep_alive() :
+def keep_alive():  
     t = Thread(target=run)
     t.start()
